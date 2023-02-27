56c5e458965129ca9ab4a8c2a915d5c215241cde
DecerZz <59297516+DecerZz@users.noreply.github.com>
2023-02-27 10:37:43 +0200

    Update RUNPF.lua

--- Games/RUNPF.lua
+++ Games/RUNPF.lua
@@ -1,6 +1,25 @@
-if not syn then -- Phantom Forces uses Actors
-    game:GetService("Players").LocalPlayer:Kick("RealZzHub - Currently Synapse X only! (parallel lua support required)")
+if not syn then -- Phantom Forces uses Actors but we bypass B)
+    local zzLPlayer = game:GetService("Players").LocalPlayer
+    local zzTeleportService = game:GetService("TeleportService")
+    zzLPlayer.OnTeleport:Connect(function()
+        queue_on_teleport([[
+            local zzWorkspace = game:GetService("Workspace")
+            local zzReplicatedFirst = game:GetService("ReplicatedFirst")
+            zzReplicatedFirst.ChildAdded:Connect(function(v)
+                if v:IsA("Actor") then
+                    zzReplicatedFirst.ChildAdded:Wait()
+                    for _,b in pairs(v:GetChildren()) do
+                        b.Parent = zzReplicatedFirst
+                    end
+                end
+            end)
+            repeat wait() until game:IsLoaded() and zzWorkspace:FindFirstChild("MenuLobby")
+            game:HttpGet("https://raw.githubusercontent.com/RealZzHub/MainV2/main/Games/PhantomForces.lua", true)
+        ]])
+    end)
+    zzTeleportService:TeleportToPlaceInstance(game.PlaceId, game.JobId)
+else
+    local Actor = getactors()[1]
+    local Script = game:HttpGet("https://raw.githubusercontent.com/RealZzHub/MainV2/main/Games/PhantomForces.lua", true)
+    syn.run_on_actor(Actor, Script)
 end
-local Actor = getactors()[1]
-local Script = game:HttpGet("https://raw.githubusercontent.com/RealZzHub/MainV2/main/Games/PhantomForces.lua", true)
-syn.run_on_actor(Actor, Script)
