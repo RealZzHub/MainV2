11ebcfae3bdf8917f283dc69c9253ac03c2a0090
DecerZz <59297516+DecerZz@users.noreply.github.com>
2023-02-27 10:44:00 +0200

    Update RUNPF.lua

--- Games/RUNPF.lua
+++ Games/RUNPF.lua
@@ -1,10 +1,11 @@
-if not syn then -- Phantom Forces uses Actors but we bypass B)
+if not syn then -- Phantom Forces uses Actors but we bypass B), modified v3rm find (wont take credit)
     local zzLPlayer = game:GetService("Players").LocalPlayer
     local zzTeleportService = game:GetService("TeleportService")
     zzLPlayer.OnTeleport:Connect(function()
         queue_on_teleport([[
             local zzWorkspace = game:GetService("Workspace")
             local zzReplicatedFirst = game:GetService("ReplicatedFirst")
+            local zzRunService = game:GetService("RunService")
             zzReplicatedFirst.ChildAdded:Connect(function(v)
                 if v:IsA("Actor") then
                     zzReplicatedFirst.ChildAdded:Wait()
@@ -13,6 +14,20 @@ if not syn then -- Phantom Forces uses Actors but we bypass B)
                     end
                 end
             end)
+
+            local old
+            old = hookmetamethod(zzRunService.Stepped, "__index", function(self, index)
+                local indexed = old(self, index)
+                if index == "ConnectParallel" and not checkcaller() then
+                    hookfunction(indexed, newcclosure(function(signal, callback)
+                        return old(self, "Connect")(signal, function()
+                            return self:Wait() and callback()
+                        end)
+                    end))
+                end
+                return indexed
+            end)
+
             repeat wait() until game:IsLoaded() and zzWorkspace:FindFirstChild("MenuLobby")
             game:HttpGet("https://raw.githubusercontent.com/RealZzHub/MainV2/main/Games/PhantomForces.lua", true)
         ]])
