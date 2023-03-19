8a3fb3cceaa04a225cc78a79adddae40902239fc
DecerZz <59297516+DecerZz@users.noreply.github.com>
2023-03-19 17:54:33 +0200

    Update RUNPF.lua

--- Games/RUNPF.lua
+++ Games/RUNPF.lua
@@ -1,4 +1,4 @@
-if not syn then -- Phantom Forces uses Actors but we bypass B), modified v3rm find (wont take credit)
+if not syn and not (KRNL_LOADED or fluxus) then -- Phantom Forces uses Actors but we bypass B), modified v3rm find (wont take credit)
 	local zzLPlayer = game:GetService("Players").LocalPlayer
 	local zzTeleportService = game:GetService("TeleportService")
 	local zzWorkspace = game:GetService("Workspace")
@@ -33,8 +33,10 @@ if not syn then -- Phantom Forces uses Actors but we bypass B), modified v3rm fi
 	]])
 	wait(0.2)
 	zzTeleportService:TeleportToPlaceInstance(game.PlaceId, game.JobId)
-else
+elseif syn and syn.run_on_actor then
     local Actor = getactors()[1]
     local Script = game:HttpGet("https://raw.githubusercontent.com/RealZzHub/MainV2/main/Games/PhantomForces.lua", true)
     syn.run_on_actor(Actor, Script)
-end 
+else
+    game:GetService("Players").LocalPlayer:Kick("RealZzHub | Unsupported Executor")
+end
