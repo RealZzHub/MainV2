fbd20e5fbbb95a0e875883b3ec4713d5c6cc1715
DecerZz <59297516+DecerZz@users.noreply.github.com>
2024-02-21 19:16:52 +0200

    Update RUNPF.lua

--- Games/RUNPF.lua
+++ Games/RUNPF.lua
@@ -4,5 +4,5 @@ repeat wait() until game:IsLoaded()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/RealZzHub/MainV2/main/Games/PhantomForces.lua", true))()
 ]])
 wait(0.2)
-zzTeleportService:TeleportToPlaceInstance(game.PlaceId, game.JobId)
+game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId)
 
